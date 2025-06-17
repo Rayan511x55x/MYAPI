8812e7e25ff0a806bf2a24d6773323a0651ad53a
Rayan511x55x <138740710+Rayan511x55x@users.noreply.github.com>
2025-06-18 00:49:15 +0300

    Update api.py

--- api.py
+++ api.py
@@ -42,4 +42,5 @@ def get_logs():
     return jsonify(data)
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # تستخدم بورت من متغير البيئة أو الافتراضي 10000Add commentMore actions
+    app.run(host="0.0.0.0", port=port)
